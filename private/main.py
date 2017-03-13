6934ea50ff71978539ea6090eac6cd5ee3b7f96c
Michael.X <xxnet.dev@gmail.com>
2017-03-13 11:18:20 +0800

    update for new p4a and work for Android 5.

--- private/main.py
+++ private/main.py
@@ -1,44 +1,10 @@
 #!/usr/bin/kivy
 
 import os, sys
-import threading
 import zipfile
-
-import kivy                                                                                     
-from kivy.app import App                                                                        
-from kivy.lang import Builder                                                                   
-from kivy.utils import platform                                                                 
-from kivy.uix.widget import Widget                                                              
-from kivy.clock import Clock                                                                    
-from jnius import autoclass                                                                     
-from android.runnable import run_on_ui_thread                                                   
 
 
 current_path = os.path.dirname(os.path.abspath(__file__))
-
-WebView = autoclass('android.webkit.WebView')                                                   
-WebViewClient = autoclass('android.webkit.WebViewClient')                                       
-activity = autoclass('org.renpy.android.PythonActivity').mActivity                              
-
-
-class Wv(Widget):                                                                               
-    def __init__(self, **kwargs):                                                               
-        super(Wv, self).__init__(**kwargs)                                                      
-        Clock.schedule_once(self.create_webview, 0)                                             
-
-    @run_on_ui_thread                                                                           
-    def create_webview(self, *args):                                                            
-        webview = WebView(activity)                                                             
-        webview.getSettings().setJavaScriptEnabled(True)                                        
-        wvc = WebViewClient();                                                                  
-        webview.setWebViewClient(wvc);                                                          
-        activity.setContentView(webview)                                                        
-        webview.loadUrl('http://127.0.0.1:8085')
-
-
-class ServiceApp(App):                                                                          
-    def build(self):                                                                            
-        return Wv()
 
 
 def get_sdcard_path():
@@ -74,7 +40,7 @@ def load_xxnet():
     if not os.path.exists(xxnet_path):
         os.mkdir(xxnet_path)
 
-    launcher_path = os.path.join(xxnet_path, "launcher")
+    launcher_path = os.path.join(xxnet_path, "code", "default", "launcher")
     if not os.path.exists(launcher_path):
         default_zip = os.path.join(current_path, "default.zip")
         with zipfile.ZipFile(default_zip, "r") as dz:
@@ -87,15 +53,11 @@ def load_xxnet():
 
 
 
-def start_xxnet():
-    th = threading.Thread(target=load_xxnet)
-    th.start()
-
-
-
 def main():
-    start_xxnet()
-    ServiceApp().run()
+    ldpath = os.environ.get("LD_LIBRARY_PATH")
+    print "ldpath:", ldpath
+    load_xxnet()
+
 
 
 if __name__ == '__main__':
--- script/build.sh
+++ script/build.sh
@@ -1,8 +1,50 @@
 #!/bin/bash
 
-./toolchain.py create --dist_name=xxnet.android --bootstrap=sdl2 --requirements=sdl2,python2,openssl,pyopenssl,cryptography,pyjnius,kivy,android
+# This script should run under xxnet-anroid path
 
-./toolchain.py apk  --package xxnet.net --name XX-Net --version 3.0.0 --private private \
---permission ACCESS_NETWORK_STATE --permission INTERNET --permission WRITE_EXTERNAL_STORAGE --permission BATTERY_STATS \
---icon default/launcher/web_ui/img/logo.png \
---presplash default/launcher/web_ui/img/logo.png
+# install system package:
+sudo dpkg --add-architecture i386
+sudo apt-get update
+sudo apt-get install -y build-essential ccache git zlib1g-dev python2.7 python2.7-dev libncurses5:i386 libstdc++6:i386 zlib1g:i386 openjdk-7-jdk unzip ant
+sudo apt-get install python-sh python-appdirs virtualenv cython python-jinja2 pkg-config autoconf automake libtool zip
+
+git clone https://github.com/xx-net/XX-Net.git
+
+# pack default xx-net python code to private path
+mkdir -p default_code/code/default
+cp -r XX-Net/code/default/gae_proxy XX-Net/code/default/launcher \
+ XX-Net/code/default/x_tunnel XX-Net/code/default/version.txt\
+ default_code/code/default/.
+mkdir -p default_code/code/default/python27/1.0/lib
+cp -r XX-Net/code/default/python27/1.0/lib/noarch \
+ default_code/code/default/python27/1.0/lib/.
+find default_code |grep "\.pyc" |xargs rm
+rm private/default.zip
+
+cd default_code
+zip -r ../private/default.zip *
+cd ..
+
+
+export ANDROIDSDK=~/android-sdk-linux
+export ANDROIDNDK=~/android-ndk-r11b
+export ANDROIDAPI=14
+export ANDROIDNDKVER=r11b
+
+
+python ../python-for-android/pythonforandroid/toolchain.py create \
+--dist_name=webview.xxnet.android --bootstrap=webview \
+--requirements=python2,openssl,pyopenssl,cryptography,pyjnius,cffi
+
+
+python ../python-for-android/pythonforandroid/toolchain.py apk \
+--dist_name=webview.xxnet.android \
+ --package xxnet.net --name XX-Net --version 3.1.0 \
+--private private \
+ --permission ACCESS_NETWORK_STATE \
+ --permission INTERNET \
+ --permission WRITE_EXTERNAL_STORAGE \
+ --permission BATTERY_STATS \
+--icon default_code/code/default/launcher/web_ui/img/logo.png \
+--presplash default_code/code/default/launcher/web_ui/img/logo.png \
+ --port=8085
